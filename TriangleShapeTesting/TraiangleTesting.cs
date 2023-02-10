3b29bddaeb94f433d8ef58c651c5ee468ec8f165
Rajan <you@example.com>
2023-02-10 17:25:56 -0500

    Commit second

--- TriangleShapeTesting/TraiangleTesting.cs
+++ TriangleShapeTesting/TraiangleTesting.cs
@@ -57,6 +57,51 @@ namespace TriangleShapeTesting
             string actual = t1.AnalyzeTriangle(9, 13, 15);
             Assert.AreEqual(expected, actual);
         }
+        [Test]
+        public void ScaleneTriangleTest6()
+        {
+            Triangle t1 = new Triangle();
+            String expected = "Based on all three sides being different, the type of triangle is a SCALENE";
+            string actual = t1.AnalyzeTriangle(21, 22, 23);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void ScaleneTriangleTest7()
+        {
+            Triangle t1 = new Triangle();
+            String expected = "Based on all three sides being different, the type of triangle is a SCALENE";
+            string actual = t1.AnalyzeTriangle(3, 4, 5);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void ScaleneTriangleTest8()
+        {
+            Triangle t1 = new Triangle();
+            String expected = "Based on all three sides being different, the type of triangle is a SCALENE";
+            string actual = t1.AnalyzeTriangle(7, 13, 9);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void ScaleneTriangleTest9()
+        {
+            Triangle t1 = new Triangle();
+            String expected = "Based on all three sides being different, the type of triangle is a SCALENE";
+            string actual = t1.AnalyzeTriangle(18, 19, 20);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void ZeroLentghTest10()
+        {
+            Triangle t1 = new Triangle();
+            String expected = "At least one side of your triangle has a zero length and is thus invalid";
+            string actual = t1.AnalyzeTriangle(0, 0, 0);
+            Assert.AreEqual(expected, actual);
+        }
+
 
 
 
